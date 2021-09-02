bcae4cc4308457a3d5061286b51ce65f94d31d61
javier <javier.slimtech@outlook.com>
2021-09-02 18:07:02 -0500

    Diseño del modulo de matematicas, registro
    Signed-off-by: javier <javier.slimtech@outlook.com>

--- controlador/primero.php
+++ controlador/primero.php
@@ -1,3 +1,26 @@
 <?php
     $pageTitle = "Primer Grado";
+
+    $bd = new BD();
+    
+    /*
+    $usuario= new Usuario();
+   
+    
+    if(isset($_POST['registrarse'])){
+        
+        $usuario->setNombre($_POST['nombre']);
+        $usuario->setApellidos($_POST['apellidos']);
+        $usuario->setFechanacimiento($_POST['fechanacimiento']);
+        $usuario->setCorreo($_POST['correo']);
+        $usuario->setTelefono($_POST['telefono']);
+        $usuario->setUsuario($_POST['usuario']);
+        $usuario->setContrasena($_POST['contrasena']);
+        
+        $bd->registrarUsuario($usuario);
+        header('Location:../vista/primero');
+        
+    } 
+    */
+    
 ?>
--- modelo/BD.php
+++ modelo/BD.php
@@ -9,10 +9,28 @@
             	echo "error";
             }
             else
-            {
-            	echo "conexion exitosa";
+            {   
+            	//echo "conexion exitosa";
             }       
         }	
+        public function registrarUsuario($usuario)
+        {   
+            if ($usuario->getId() == -1) {    
+            $datos="'".$usuario->getNombre()."',";
+            $datos.= "'".$usuario->getApellidos()."',";
+            $datos.= "'".$usuario->getEdad()."',";
+            $datos.= "'".$usuario->getCorreo()."',";
+            $datos.= "'".$usuario->getTelefono()."',";
+            $datos.= "'".$usuario->getUsuario()."',";
+            $datos.= "'".$usuario->getContrasena()."'";
+
+            $insertar = "INSERT INTO usuario (nombre, apellidos, edad, correo, telefono, usuario, contrasena) VALUES($datos)";
+            echo $insertar;
+            $this->conexion->query($insertar);
+
+            }
+       
+        } 
     } 
 
 ?>
