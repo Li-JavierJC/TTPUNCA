3c15e6aaec11086d1ad5101dc10e13ba3be86a1e
javier <javier.slimtech@outlook.com>
2021-09-06 18:02:59 -0500

    Diseño de memorama, desarrollo javascript
    Signed-off-by: javier <javier.slimtech@outlook.com>

---
+++ vista/js/espanol-juego1.js
@@ -0,0 +1,42 @@
+var arreglo= new Array();
+var arregloJugar= [];
+var contador= 0;
+var contador_fallos=0;
+var contador_aciertos=0;
+var contador_tiempo=59;
+var aleatorio;
+var ciclo_tiempo;
+
+window.onload= function{
+    for(i=1; i<=4; i++){
+        for(j=0; j>=0; j++){
+            aleatorio=Math.floor(Math.random()*8+1);
+            if(arreglo[aleatorio]==undefined){
+                document.getElementById('c'+ aleatorio).style.backgroundImage='url(../../vista/images/' + i +')';
+                document.getElementById("c" + aleatorio).style.pointerEvents="none";
+                contador++;
+            }
+            if(contador== 2){
+                break;
+            }
+        }
+        contador=0;
+    }
+    fondo();
+    funcion_tiempo();
+}
+
+function fondo(){
+   var ciclo= setInterval(function(){
+       for(i=1; i<=8; i++){
+           document.getElementById('carta'+i).style.backgroundImage='url(../../vista/images/fondo.jpg)';
+           document.getElementById('c'+i).style.pointerEvents= "auto";
+       }
+       clearInterval(ciclo);
+   }, 3000); 
+}
+
+
+
+
+
