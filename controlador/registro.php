20395e71c06ebd6e8468d904c82f472fe9c6f17b
javier <javier.slimtech@outlook.com>
2021-09-07 18:04:11 -0500

    Desarrollo del juego de memorama
    Signed-off-by: javier <javier.slimtech@outlook.com>

--- controlador/registro.php
+++ controlador/registro.php
@@ -1,21 +1,22 @@
 <?php
     $pageTitle = "Registrate";
     
-    
     $bd = new BD();
     $usuario= new Usuario();
-   
+    echo "llego";
+
     if(isset($_POST['registrarse'])){
-        
+        echo "entra";
+
         $usuario->setUsuario($_POST['usuario']);
         $usuario->setContrasena($_POST['contrasena']);
-    
+
         $bd->registrarUsuario($usuario);
         
         header('Location: primero');
+        echo "hola";
         
     } 
-
-    
+   
     
 ?>
